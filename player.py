5a25df04a862234ae67f9cff0f05456de9154c4f
GothicLogic <iammorski@gmail.com>
2025-08-05 08:39:26 +0100

    Update bullets to shoot with a rate limit

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 from circleshape import CircleShape
 from shot import Shot
 
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, -1).rotate(self.rotation)
@@ -24,21 +25,24 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
         
     def shoot(self):
-        forward = pygame.Vector2(0, -1).rotate(self.rotation)
-        velocity = forward * PLAYER_SHOOT_SPEED
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity = velocity
+        if self.timer <= 0:
+            forward = pygame.Vector2(0, -1).rotate(self.rotation)
+            velocity = forward * PLAYER_SHOOT_SPEED
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = velocity
+            self.timer = PLAYER_SHOOT_COOLDOWN
 
     def update(self, dt):
+        self.timer = max(0, self.timer - dt)
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.move(-dt)
-        if keys[pygame.K_s]:
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             self.move(dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
