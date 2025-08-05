467b5b228c41b7b2b53fb9e392554500a5b3914a
GothicLogic <iammorski@gmail.com>
2025-08-05 09:17:06 +0100

    Add asteroid splitting based on size

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        velocity1 = self.velocity.rotate(random_angle) * 1.2
+        velocity2 = self.velocity.rotate(-random_angle) * 1.2
+        radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, radius)
+
+        asteroid1.velocity = velocity1
+        asteroid2.velocity = velocity2
